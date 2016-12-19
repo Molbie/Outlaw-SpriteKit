dd61cb1dba1eb5312e9297fbb664f9c559a8947e
Brian Mullen <bmullen@molbie.co>
2016-12-19 00:06:20 -0700

    Add SKReachConstraints

---
+++ OutlawSpriteKitTests/Nodes/Constraints/SKReachConstraintsTests.swift
@@ -0,0 +1,106 @@
+//
+//  SKReachConstraintsTests.swift
+//  OutlawSpriteKit
+//
+//  Created by Brian Mullen on 12/18/16.
+//  Copyright © 2016 Molbie LLC. All rights reserved.
+//
+
+import XCTest
+import Outlaw
+import OutlawCoreGraphics
+import OutlawSimd
+import SpriteKit
+@testable import OutlawSpriteKit
+
+
+@available(OSX 10.10, *)
+class SKReachConstraintsTests: XCTestCase {
+    func testExtractableValue() {
+        typealias keys = SKReachConstraints.ExtractableKeys
+        
+        let rawData: [String: CGFloat] = [keys.lowerAngleLimit: 30,
+                                          keys.upperAngleLimit: 60]
+        let data: [String: [String: CGFloat]] = ["data": rawData]
+        let value: SKReachConstraints = try! data.value(for: "data")
+        
+        XCTAssertEqual(value.lowerAngleLimit, rawData[keys.lowerAngleLimit])
+        XCTAssertEqual(value.upperAngleLimit, rawData[keys.upperAngleLimit])
+    }
+    
+    func testIndexExtractableValue() {
+        typealias indexes = SKReachConstraints.ExtractableIndexes
+        
+        var rawData = [CGFloat](repeating: 0, count: 2)
+        rawData[indexes.lowerAngleLimit] = 30
+        rawData[indexes.upperAngleLimit] = 60
+        
+        let data: [[CGFloat]] = [rawData]
+        let value: SKReachConstraints = try! data.value(for: 0)
+        
+        XCTAssertEqual(value.lowerAngleLimit, rawData[indexes.lowerAngleLimit])
+        XCTAssertEqual(value.upperAngleLimit, rawData[indexes.upperAngleLimit])
+    }
+    
+    func testInvalidValue() {
+        let rawData: String = "Hello, Outlaw!"
+        let data: [String] = [rawData]
+        
+        let ex = self.expectation(description: "Invalid data")
+        do {
+            let _: SKReachConstraints = try data.value(for: 0)
+        }
+        catch {
+            if case OutlawError.typeMismatchWithIndex = error {
+                ex.fulfill()
+            }
+        }
+        self.waitForExpectations(timeout: 1, handler: nil)
+    }
+    
+    func testSerializable() {
+        typealias keys = SKReachConstraints.ExtractableKeys
+        
+        let value = SKReachConstraints(lowerAngleLimit: 30, upperAngleLimit: 60)
+        let data: [String: CGFloat] = value.serialized()
+        
+        XCTAssertEqual(data[keys.lowerAngleLimit], value.lowerAngleLimit)
+        XCTAssertEqual(data[keys.upperAngleLimit], value.upperAngleLimit)
+    }
+    
+    func testIndexSerializable() {
+        typealias indexes = SKReachConstraints.ExtractableIndexes
+        
+        let value = SKReachConstraints(lowerAngleLimit: 30, upperAngleLimit: 60)
+        let data: [CGFloat] = SKReachConstraints.serialize(value)
+        
+        XCTAssertEqual(data[indexes.lowerAngleLimit], value.lowerAngleLimit)
+        XCTAssertEqual(data[indexes.upperAngleLimit], value.upperAngleLimit)
+    }
+    
+    func testUpdatable() {
+        typealias keys = SKReachConstraints.ExtractableKeys
+        
+        let data: [String: CGFloat] = [keys.lowerAngleLimit: 30,
+                                       keys.upperAngleLimit: 60]
+        let value = SKReachConstraints(lowerAngleLimit: 0, upperAngleLimit: 1)
+        try! value.update(with: data)
+        
+        XCTAssertEqual(value.lowerAngleLimit, data[keys.lowerAngleLimit])
+        XCTAssertEqual(value.upperAngleLimit, data[keys.upperAngleLimit])
+    }
+    
+    func testIndexUpdatable() {
+        typealias indexes = SKReachConstraints.ExtractableIndexes
+        
+        var data = [CGFloat](repeating: 0, count: 2)
+        data[indexes.lowerAngleLimit] = 30
+        data[indexes.upperAngleLimit] = 60
+        
+        let value = SKReachConstraints(lowerAngleLimit: 0, upperAngleLimit: 1)
+        try! value.update(with: data)
+        
+        XCTAssertEqual(value.lowerAngleLimit, data[indexes.lowerAngleLimit])
+        XCTAssertEqual(value.upperAngleLimit, data[indexes.upperAngleLimit])
+    }
+}
